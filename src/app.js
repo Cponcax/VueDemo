1120b0742bd7f0e66b889461ac782ffd85853098
liuzhimin <liuzm@proudkids.cn>
2018-07-18 18:27:13 +0800

    layout redesign

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ Vue.use(VueAuth, {
   },
   http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
   router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
-  loginData: { url: 'https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin/user/login', fetchUser: false },
+  loginData: { url: 'http://127.0.0.1:8089/oauth/token', fetchUser: false },
   refreshData: { enabled: false }
 })
 
